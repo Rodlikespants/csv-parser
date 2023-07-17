740b3b81b2b6ca642cb54996b48d0bf19f8ed3bb
Rodlikespants <rodwittlai@gmail.com>
2023-07-17 17:12:59 -0400

    Initial commit

---
+++ src/main/java/com/automationrhapsody/reststub/config/CsvParserConfig.java
@@ -0,0 +1,21 @@
+package com.automationrhapsody.reststub.config;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+
+import javax.validation.constraints.NotEmpty;
+
+public class CsvParserConfig extends Configuration {
+    @NotEmpty
+    private String version;
+
+    @JsonProperty
+    public String getVersion() {
+        return version;
+    }
+
+    @JsonProperty
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
